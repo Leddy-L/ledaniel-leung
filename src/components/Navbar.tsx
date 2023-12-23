71ea604ce7f9b140b8a458cadc840df711290b13
Asianu <Asianu@users.noreply.github.com>
2023-12-22 23:53:31 -0800

    Cleaned up list

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import "../styles/Navbar.scss";
 import logo from "../logo.svg";
 
 function Navbar() {
+  const navItems = ["About", "Experience", "Projects"];
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
       <a className="navbar-brand" href="#">
@@ -13,21 +15,13 @@ function Navbar() {
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Experience
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Projects
-            </a>
-          </li>
+          {navItems.map((item) => (
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                {item}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
