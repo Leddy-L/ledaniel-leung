87ebbee6b6cb297cc6da10d2c94e20eb9a71cc82
Asianu <Asianu@users.noreply.github.com>
2023-12-28 13:41:24 -0800

    Migrate to react-bootstrap and fix Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,33 @@
 import "../styles/Navbar.scss";
 import logo from "../logo.svg";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-function Navbar() {
+function CustomNavbar() {
   const navItems = ["About", "Experience", "Projects"];
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <a className="navbar-brand" href="#">
-        <img src={logo} width="60" height="60" alt="" />
-      </a>
-      <button className="navbar-toggler" type="button">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ms-auto">
-          {navItems.map((item) => (
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                {item}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="bg-light" fixed="top">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <a className="navbar-brand" href="#">
+            <img src={logo} width="60" height="60" alt="" />
+          </a>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto justify-content-center">
+            {navItems.map((item) => (
+              <Nav.Item>
+                <Nav.Link href="#">{item}</Nav.Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default CustomNavbar;
