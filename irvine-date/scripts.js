b7e35a77758ae4968fd45ba587e24ff18cfccb2d
Asianu <Asianu@users.noreply.github.com>
2024-03-30 00:58:30 -0700

    some functions and photos added.

--- irvine-date/scripts.js
+++ irvine-date/scripts.js
@@ -3,12 +3,21 @@ let current_stage = 0;
 
 $(document).ready(function () {
   function nextStage() {
-    console.log(stage[current_stage]);
     $(stage[current_stage++]).hide();
+    $(stage[current_stage]).css("display", "flex");
+  }
+
+  function reset() {
+    $(stage[current_stage]).hide();
+    current_stage = 0;
     $(stage[current_stage]).css("display", "flex");
   }
 
   $(".next").on("click", function () {
     nextStage();
+  });
+
+  $(".reset").on("click", function () {
+    reset();
   });
 });
