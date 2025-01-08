bc74341b1893b60a560c9544243a9ba399d44369
LeDaniel Leung <ledaniel.leung@gmail.com>
2025-01-08 23:51:04 +0900

    Added some text for the cards.

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,17 +1,43 @@
 import { Card } from "flowbite-react";
-const Education = () => {
 
-  return (
-    <div>
-      <h1>Education</h1>
-      <Card className="bg-zinc-900">
-          <div>
-            <h1>University of California, San Diego</h1>
-            <p>Some text</p>
-          </div>
-      </Card>
-    </div>
-  );
+interface Experience {
+    id: Number,
+    name: String,
+    logo?: ImageData,
+    years: String,
+    major: String,
+    minor: String
+    location: String,
+    description: String,
+    icons?: Array<ImageData>
+}
+
+const Education = () => {
+const education = {
+    id: 0,
+    name: "University of California, San Diego",
+    years: "2015 - 2019",
+    major: "Major: Mathematics - Computer Science",
+    minor: "Minor: Cognitive Science (Human Computer Interaction)",
+    location: "San Diego, CA.",
+    description: "I learned. A lot."
+}
+
+    return (
+        <div>
+            <h1>Education</h1>
+            <Card className="bg-zinc-900">
+                <div>
+                    <h1>{education.name}</h1>
+                    <h2>{education.years}</h2>
+                    <h2>{education.major}</h2>
+                    <h2>{education.minor}</h2>
+                    <h2>{education.location}</h2>
+                    <p>{education.description}</p>
+                </div>
+            </Card>
+        </div>
+    );
 };
 
 export default Education;
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,15 +1,47 @@
 import { Card } from "flowbite-react";
+
+interface Experience {
+  id: Number,
+  name: String,
+  logo?: ImageData,
+  years: String,
+  title: String,
+  location: String,
+  description: String,
+  technologies?: Array<ImageData>
+}
+
 const Experience = () => {
-  const workExperience = ["Qualcomm", "Northrop Grumman"];
+  const workExperience = [
+    {
+      id: 0,
+      name: "Qualcomm",
+      years: "2022 - Present",
+      title: "Software Engineer",
+      location: "San Diego, CA.",
+      description: "At Qualcomm, I supported the Software Automation and Tools (QSWAT) team by implementing new features, optimizing legacy applications, and supporting users daily, with the following technologies:"
+    }, 
+    {
+      id: 1,
+      name: "Northrop Grumman",
+      years: "2019 - 2022",
+      title: "Systems Engineer",
+      location: "San Diego, CA.",
+      description: "As an intern-to-fulltime hire, I worked on various middleware applications that supported the communications systems on avionic vehicles."
+    }
+  ];
 
   return (
     <div>
       <h1>Experience</h1>
       <Card className="bg-zinc-900">
-        {workExperience.map((exp_item) => (
+        {workExperience.map((workExperience: Experience) => (
           <div>
-            <h1>{exp_item}</h1>
-            <p>Some text</p>
+            <h1>{workExperience.name}</h1>
+            <h2>{workExperience.years}</h2>
+            <h2>{workExperience.title}</h2>
+            <h2>{workExperience.location}</h2>
+            <p>{workExperience.description}</p>
           </div>
         ))}
       </Card>
