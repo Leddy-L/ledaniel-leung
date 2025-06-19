12561bcda85bdff49fa1fea4fa17a16463017799
LeDaniel Leung <ledaniel.leung@gmail.com>
2025-06-19 00:16:59 -0700

    Changes to address eslint issues during build

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
-import Navigation from "./components/navigation/Navigation";
-import PageBanner from "./components/navigation/PageBanner";
-import About from "./sections/About";
-import Experience from "./sections/Experience";
-import Projects from "./sections/Projects";
-import PageFooter from "./components/navigation/Footer";
-import Education from "./sections/Education";
+import About from './sections/About'
+import Education from './sections/Education'
+import Experience from './sections/Experience'
+import Navigation from './components/navigation/Navigation'
+import Projects from './sections/Projects'
+import PageFooter from './components/navigation/Footer'
 
-import "./styles/index.css";
+import './styles/index.css'
 
 function App() {
-  return (
-    <div className="bg-zinc-900 text-zinc-200">
-      <Navigation />
-      {/* below should be a "page" and each page can be created of components */}
-      <div className="p-10 mt-16 w-full md:w-4/5 md:ml-[20%]">
-        {/* <PageBanner /> */}
-        <div className="flex flex-col space-y-8">
-          <About />
-          <Experience />
-          <Projects />
-          <Education />
-          <PageFooter />
+    return (
+        <div className="bg-zinc-900 text-zinc-200">
+            <Navigation />
+            <div className="p-10 mt-16 w-full md:w-4/5 md:ml-[20%]">
+                <div className="flex flex-col space-y-8">
+                    <About />
+                    <Experience />
+                    <Projects />
+                    <Education />
+                    <PageFooter />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
